886955eac90c5d6a70e226016d88896cc3d14590
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-22 13:44:01 -0700

    Adding SQLite functionality

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import flask
 
 app = flask.Flask(__name__)
@@ -7,6 +9,29 @@ SAMPLE_QUESTIONS = (
     'What is your favorite food?',
     'Do you have a nickname? If so, what is it?',
 )
+
+# Create an in-memory SQLite DB with toy data, just for now.
+CONNECTION = sqlite3.connect(':memory:')
+
+with CONNECTION:
+    cursor = CONNECTION.cursor()
+
+    # Create some tables.
+    cursor.execute('''CREATE TABLE questions (
+        id          INTEGER PRIMARY KEY,
+        question    TEXT
+    )''')
+
+    cursor.execute('''CREATE TABLE answers (
+        question_id                 INTEGER NOT NULL,
+        user_id                     INTEGER PRIMARY KEY,
+        answer                      TEXT,
+        FOREIGN KEY(question_id)    REFERENCES questions(id)
+    )''')
+
+    # Add sample questions to the DB.
+    # Generator expression looks funky, since each question needs to be a 1-tuple.
+    cursor.executemany("INSERT INTO questions(question) VALUES (?)", ((q,) for q in SAMPLE_QUESTIONS))
 
 @app.route('/')
 def quiz():
