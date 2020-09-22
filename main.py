1c321341c60cfdfe3b490ef119efe971d1f43646
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-22 13:19:56 -0700

    Now able to display sample questions in a form, submit not yet implemented

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+SAMPLE_QUESTIONS = (
+    'What is your name?',
+    'What is your favorite food?',
+    'Do you have a nickname? If so, what is it?',
+)
 
 @app.route('/')
 def quiz():
-    return '<h1>Quiz Here</h1>'
+    return flask.render_template('main.html', id_and_questions=enumerate(SAMPLE_QUESTIONS))
 
 if __name__ == '__main__':
     app.run(debug=True)
